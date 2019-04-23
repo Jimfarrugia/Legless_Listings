6b05f2c6504c38de5ba114cb8e8f7ee4433cc3e4
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-04-23 14:24:13 +1000

    added devise gem and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path: "/", path_names: { sign_in: "login", sign_out: "logout" }
   get "/", to: "pages#home", as: "root"
   resources :listings
 end
